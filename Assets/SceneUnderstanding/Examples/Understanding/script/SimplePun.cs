af33cebe13d0621606b16287077b8842724189be
mamebou <mamebou0810@outlook.jp>
2022-05-16 02:20:18 +0900

    create online function

--- Assets/SceneUnderstanding/Examples/Understanding/script/SimplePun.cs
+++ Assets/SceneUnderstanding/Examples/Understanding/script/SimplePun.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using Photon.Pun;
 using Photon.Realtime;
 using System;
+using TMPro;
+using Microsoft.MixedReality.SceneUnderstanding.Samples.Unity;
+using Microsoft.MixedReality.SceneUnderstanding;
 
 public class SimplePun : MonoBehaviourPunCallbacks {
 
@@ -10,11 +13,38 @@ public class SimplePun : MonoBehaviourPunCallbacks {
     private String playerName;
     public GameObject stateManager;
     int playerNum;
+    bool isStart = true;
+    bool isStarted = false;
+    Player enemy = null;
+    Player player = null;
+    public float CountDown = 5f;
+    TextMeshPro CountDownText;
+    GameObject SceneUnderstanding;
+    public int MaxPlayer = 2;
+
 
     // Use this for initialization
     void Start () {
         playerName = CanasController.getPlayerName();
+        CountDownText = GameObject.Find("CountDwon").GetComponent<TextMeshPro>();
+        SceneUnderstanding = GameObject.Find("SceneUnderstandingManager");
+    }
 
+    void Update(){
+        if(isStart){
+          if((bool)player.CustomProperties["isReady"] && (bool)enemy.CustomProperties["isReady"]){
+              if(!isStarted){
+                  CountDown -= Time.deltaTime;
+                  CountDownText.text = CountDown.ToString("F2");
+                }
+              
+              if(CountDown <= 0f){
+                  SceneUnderstanding.GetComponent<SceneUnderstandingManager>().DisplayScanPlanes = true;
+                  isStart = false;
+                  isStarted = true;
+                }
+            }
+        }
     }
 
     void OnGUI()
@@ -49,22 +79,41 @@ public class SimplePun : MonoBehaviourPunCallbacks {
     //ルームに入室後に呼び出される
     public override void OnJoinedRoom(){
         Player[] players = PhotonNetwork.PlayerListOthers;
-        Player player = PhotonNetwork.LocalPlayer;
+        if(players.Length != 0){
+            enemy = players[0];
+            isStart = true;
+        }
+        player = PhotonNetwork.LocalPlayer;
         ExitGames.Client.Photon.Hashtable properties = new ExitGames.Client.Photon.Hashtable();
         properties["playerName"] = "hello";
+        properties["isReady"] = false;
         PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
     }
 
     //カスタムプロパティがセットされたとき
     public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
     {
-        Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["playerName"]);
-        Player[] players = PhotonNetwork.PlayerListOthers;
-        Player enemy = null;
-        if(players.Length != 0){
-            enemy = players[0];
-        }
+        // Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["playerName"]);
+        // Debug.Log(changedProps);
+        // Player[] players = PhotonNetwork.PlayerListOthers;
+        // Player enemy = null;
+        // if(players.Length != 0){
+        //     enemy = players[0];
+        //     isStart = true;
+        // }
+    }
 
-        Debug.Log(enemy);
+    //ほかのプレイヤーが参加時
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if(PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayer){
+            Player[] players = PhotonNetwork.PlayerListOthers;
+            enemy = players[0];
+            isStart = true;
+        }
+    }
+
+    public void Ready(){
+        player.CustomProperties["isReady"] = true;
     }
 }
